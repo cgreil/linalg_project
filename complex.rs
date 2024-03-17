195740848a2719e2838c28af73475bc6c9dd5f84
christoph <c.moser400@gmail.com>
2024-03-17 21:52:37 +0100

    added tests for addition and subtraction

--- complex.rs
+++ complex.rs
@@ -19,6 +19,30 @@ mod complex {
         fn division (number1: Self, number2: Self) -> Self;
     }
     
+    impl Complex {
+    
+        pub fn new(real: f32, imaginary: f32) -> Self {
+            Complex {
+                real: real,
+                imaginary: imaginary,
+            }
+        }
+
+
+        pub fn calculateAngle(number: Self) -> f32 {
+            42.0
+        }
+
+        pub fn calculateNorm(number: Self) -> f32 {
+            ComplexArithmetic::norm(number)   
+        }   
+
+        pub fn fromPolar(Norm: f32, angle: f32) {
+            
+        }
+
+    }
+
     impl ComplexArithmetic for Complex {
 
        fn norm(number: Complex) -> f32 {
@@ -73,3 +97,62 @@ fn main() {
 
     println!("Complex number is {:?}", num);
 }
+
+
+// Unit tests
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::complex::*;
+    use assert_approx_eq::assert_approx_eq; 
+
+
+    const delta:f32 = 0.00001f32;
+    
+    #[test]
+    fn sanity_test() {
+        assert!(true);
+    }
+    
+    #[test]
+    fn addition_check() {
+        let test_num1 = Complex {
+            real: 1.0,
+            imaginary: 2.0,
+        };
+        let test_num2 = Complex {
+            real: -1.0,
+            imaginary: 4.0 
+        };
+        let actual = complex::Complex::addition(test_num1, test_num2);
+        let expected = Complex {
+            real: 0.0,
+            imaginary: 6.0,
+        };
+        assert_approx_eq!(actual.real, expected.real, delta);
+        assert_approx_eq!(actual.imaginary, expected.imaginary, delta); 
+    }
+
+    #[test]
+    fn subtraction_check() {
+        let test_num1 = Complex {
+            real: 2.0,
+            imaginary: -3.0
+        };
+        let test_num2 = Complex {
+            real: 5.0,
+            imaginary: 9999.0,
+        };
+        let actual = complex::Complex::subtraction(test_num1, test_num2);
+        let expected = Complex {
+            real: -3.0,
+            imaginary: -10002.0,
+        };
+        assert_approx_eq!(actual.real, expected.real, delta);
+        assert_approx_eq!(actual.imaginary, expected.imaginary, delta);
+    }
+}
+
+
+
